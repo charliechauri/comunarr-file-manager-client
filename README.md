2a2d0ebba3f2a6666f28e6c7ffb4ea2c122e4157
charliechauri <carlos.echauri@gmail.com>
2017-06-12 20:01:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+@todo Validate that at least one general topic is selected to create/edit a specific topic
+@todo Validate that at least one project is selected to create/edit a collective
+@todo Regex for trim at init and at the end of a name
+@todo Validate length of name property in all catalogs
+@todo content-type-manager component
+@todo Implement trademark
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+module.exports = (env = {}) => {
+    const isProduction = env.production === 'true';
+
+    const path = require('path');
+    const webpack = require('webpack');
+    const ExtractTextPlugin = require('extract-text-webpack-plugin');
+    const BabiliPlugin = require('babili-webpack-plugin');
+    const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+    const config = {
+        entry: {
+            vendor: ['angular'],
+            app: './src/app/comunarr-file-manager-client.module.js'
+        },
+        output: {
+            filename: 'scripts/bundle.js',
+            path: isProduction ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'src')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['babili']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                }
+            ]
+        },
+        devtool: (() => {
+            if (isProduction) {
+                return 'hidden-source-map';
+            }
+            else {
+                return 'eval-source-map';
+            }
+        })(),
+        devServer: {
+            contentBase: path.join(__dirname, 'src'),
+            compress: true,
+            port: 4000
+        },
+        target: 'web',
+        plugins: (() => {
+            let plugins = [
+                new webpack.optimize.CommonsChunkPlugin({
+                    name: 'vendor',
+                    filename: 'scripts/vendor.js',
+                    minChunks: Infinity
+                }),
+                new ExtractTextPlugin('css/styles.css')
+            ];
+            if (isProduction) {
+                plugins.push(new HtmlWebpackPlugin({
+                    title: 'Comunarr file manager client',
+                    filename: 'index.html',
+                    template: 'src/index.ejs',
+                    cache: true,
+                    showErrors: true
+                }));
+                plugins.push(new BabiliPlugin());
+            }
+
+            return plugins;
+        })()
+    };
+
+    return config;
+};
