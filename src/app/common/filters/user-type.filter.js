06e948ede117bc56cdfa6163059489d0c1041f71
Carlos Echauri <cechauri@tesselar.mx>
2017-07-01 23:23:16 -0500

    refactor Implements UserTypesService

--- src/app/common/filters/user-type.filter.js
+++ src/app/common/filters/user-type.filter.js
@@ -1,20 +1,9 @@
-export const UserTypeFilter = () => {
-    return type => {
-        let typeOfUser = '';
-        switch (type) {
-            case 1:
-                typeOfUser = 'Administrador';
-                break;
-            case 2:
-                typeOfUser = 'Miembro de equipo';
-                break;
-            case 3:
-                typeOfUser = 'Usuario regular';
-                break;
-            default:
-                break;
-        }
+export const UserTypeFilter = UserTypesService => {
+    'ngInject';
 
-        return typeOfUser;
+    let typesOfUser = UserTypesService.get();
+
+    return typeId => {
+        return typesOfUser.filter(type => type.id === typeId)[0].name || typesOfUser[2].name;
     };
 };
