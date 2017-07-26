98e435ba8fda5492e6b87e2a0f8756db373a5707
Carlos Echauri <carlos.echauri@gmail.com>
2017-07-26 00:05:07 -0500

    feat Minor changes to integrate to backend

--- src/app/common/components/password-handler/password-handler.component.js
+++ src/app/common/components/password-handler/password-handler.component.js
@@ -8,13 +8,14 @@ export const PasswordHandlerComponent = {
         showChangePassword: '&'
     },
     controller: class PasswordHandlerComponent {
-        constructor($scope, $mdDialog, ResponseHandler, PasswordHandler) {
+        constructor($scope, $mdDialog, ResponseHandler, PasswordHandler, localStorageService) {
             'ngInject';
 
             this.$scope = $scope;
             this.$mdDialog = $mdDialog;
             this.ResponseHandler = ResponseHandler;
             this.PasswordHandler = PasswordHandler;
+            this.localStorageService = localStorageService;
             this.form = this.newForm();
         }
 
@@ -28,12 +29,17 @@ export const PasswordHandlerComponent = {
                     template: dialogTemplate,
                     targetEvent
                 }).then(form => {
-                    const newForm = this.isAdmin ? { idUser: this.user.id, password: form.newPassword } : { password: form.password, newPassword: form.newPassword };
-
+                    let user = this.localStorageService.get('user');
+                    form.id = user.id;
+                    form.password = !this.isAdmin ? form.password : undefined;
                     this.PasswordHandler
-                        .change(newForm)
+                        .change(form)
                         .then(response => {
                             this.ResponseHandler.success(response);
+                            if (!this.isAdmin) {
+                                user.password = form.newPassword;
+                                this.localStorageService.set('user', user);
+                            }
                             this.form = this.newForm();
                         })
                         .catch(() => this.form = this.newForm());
--- src/app/common/components/password-handler/password-handler.service.js
+++ src/app/common/components/password-handler/password-handler.service.js
@@ -2,12 +2,16 @@ export class PasswordHandler {
     constructor($http, EnvironmentService) {
         'ngInject';
         this.$http = $http;
-        this.URL = `${EnvironmentService.getCurrent().BASE_URL}/password`;
+        this.BASE_URL = `${EnvironmentService.getCurrent().BASE_URL}/user/password`;
 
     }
 
+    /**
+     * If form.password is not defined, then the functionality is intended for admin users, else a user is changing its own password
+     * @param {any} form { password: string, newPassword: string, confirmPassword: string }
+     */
     change(form) {
-        this.URL = `${!!form.password ? '/own' : ''}`; // Admin or user
+        this.URL = `${this.BASE_URL + (!!form.password ? '' : '/admin')}`; // Admin or user
         return this.$http.post(this.URL, form).then(response => response.data);
     }
 }
