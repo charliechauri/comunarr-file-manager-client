4172e3de0eb097d045b80058ed6e5ae2a84bfc8a
Carlos Echauri <carlos.echauri@gmail.com>
2017-08-02 21:51:19 -0500

    fix Typo

--- src/app/common/services/menu.service.js
+++ src/app/common/services/menu.service.js
@@ -7,7 +7,7 @@ export class MenuService {
         this.allItems = [
             { label: 'Usuarios', state: 'users', privacyType: 1 },
             { label: 'Proyectos', state: 'projects', privacyType: 1 },
-            { label: 'Collectivos', state: 'collectives', privacyType: 1 },
+            { label: 'Colectivos', state: 'collectives', privacyType: 1 },
             { label: 'Tipos de contenido', state: 'content-types', privacyType: 1 },
             { label: 'Temas generales', state: 'general-topics', privacyType: 1 },
             { label: 'Temas específicos', state: 'specific-topics', privacyType: 1 },
